0204d4723c5740ff3cb1c75eec46e47699072aec
内田麻美 <mamimami49192@gmail.com>
2023-02-01 18:30:03 +0900

    init commit

---
+++ ios_training/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ios_training
+//
+//  Created by 内田麻美 on 2023/02/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
